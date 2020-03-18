619b21747c4ff5583b618038a3d66bac440f868a
fmanda <febrian.manda@gmail.com>
2020-03-18 22:58:46 +0700

    -

--- android/app/src/main/java/com/fmanda/autopartdashboard/MainActivity.java
+++ android/app/src/main/java/com/fmanda/autopartdashboard/MainActivity.java
@@ -29,14 +29,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
@@ -46,8 +46,12 @@ public class MainActivity extends AppCompatActivity {
         ).setDrawerLayout(drawer).build();
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavController navController = Navigation.findNavController(this, R.id.nav_profitLoss);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        navController.navigate(R.id.nav_profitLoss);
+//        setBackgroundColor(this.getColor(R.color.colorBackground));
     }
 
     @Override
--- android/app/src/main/java/com/fmanda/autopartdashboard/ui/profitloss/ProfitLossFragment.java
+++ android/app/src/main/java/com/fmanda/autopartdashboard/ui/profitloss/ProfitLossFragment.java
@@ -1,7 +1,11 @@
 package com.fmanda.autopartdashboard.ui.profitloss;
 
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,6 +18,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.fmanda.autopartdashboard.R;
@@ -23,6 +31,7 @@ import com.fmanda.autopartdashboard.controller.ControllerRequest;
 import com.fmanda.autopartdashboard.controller.ControllerRest;
 import com.fmanda.autopartdashboard.model.ModelProfitLoss;
 
+import java.util.Calendar;
 import java.util.List;
 
 public class ProfitLossFragment extends Fragment {
@@ -40,12 +49,45 @@ public class ProfitLossFragment extends Fragment {
         mViewModel = new ViewModelProvider(this).get(ProfitLossViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profitloss, container, false);
         rvProfit = root.findViewById(R.id.rvProfit);
-        rvProfit.addItemDecoration(new DividerItemDecoration(rvProfit.getContext(), DividerItemDecoration.VERTICAL));
+//        rvProfit.addItemDecoration(new DividerItemDecoration(rvProfit.getContext(), DividerItemDecoration.VERTICAL));
 
         profitLossAdapter = new ProfitLossAdapter(getContext(), mViewModel.groups, mViewModel.profits);
         rvProfit.setLayoutManager(new GridLayoutManager(getContext(), 1));
         rvProfit.setAdapter(profitLossAdapter);
         loadFromRest();
+
+
+
+        final Button btnExpand = root.findViewById(R.id.btnExpand);
+        btnExpand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                profitLossAdapter.isExpand = !profitLossAdapter.isExpand;
+                profitLossAdapter.notifyDataSetChanged();
+                if (profitLossAdapter.isExpand) {
+                    btnExpand.setText("+Show Details");
+                }else{
+                    btnExpand.setText("-Hide Details");
+                }
+
+//                DialogFragment newFragment = new DatePickerFragment();
+//                newFragment.show( getParentFragmentManager(), "datePicker");
+
+            }
+        });
+
+        Spinner spMonth = root.findViewById(R.id.spMonth);
+//        ArrayAdapter<CharSequence> spMonthAdapter = ArrayAdapter.createFromResource(getContext(),
+//                R.array.monts_array, android.R.layout.simple_spinner_item);
+//        spMonthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spMonth.setAdapter(spMonthAdapter);
+
+        Spinner spYear = root.findViewById(R.id.spYear);
+//        ArrayAdapter<CharSequence> spYearAdapter = ArrayAdapter.createFromResource(getContext(),
+//                R.array.years_array, android.R.layout.simple_spinner_item);
+//        spYearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spYear.setAdapter(spYearAdapter);
+
 
         return root;
     }
@@ -80,4 +122,26 @@ public class ProfitLossFragment extends Fragment {
 
         cr.SyncProfitLoss(1, 2020);
     }
+
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+    }
+
 }
